4cb09aebc7b262c31d2611b1db86e7dbf2e63f95
Barbie Ordonez <ordonezbarbie@gmail.com>
2022-11-09 16:49:22 -0700

    Explain index position

--- arrays.js
+++ arrays.js
@@ -1,5 +1,18 @@
 var catNames = ["Ash", "Spooky", "Cutie", "Sky"];
-var kidAges = [13, 12, 10, 1];
-var expressions = [4<3, 7 === 7, 17 < 100, 14>10];
+// .push() method- adds one or more elements to the end of the array.
+catNames.push("Sammy");
+console.log(catNames);
 
-// console.log(expressions);
+var kidAges = [13, 12, 10, 1];
+//.shift() method- removes the first element from an array. 
+kidAges.shift();
+console.log(kidAges);
+
+var expressions = [4<3, 7 === 7, 17 < 100, 14>10];
+// .pop() method- removes the last element from an array.
+expressions.pop();
+console.log(expressions);
+
+// Index Positions:
+// Index positions refers to where an element is on an array. It begins with 0. 
+
